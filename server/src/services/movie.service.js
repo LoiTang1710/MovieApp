f5bc359f9bea425a8de75e583e18798479e872c7
LoiTang <heobovidai@gmail.com>
2026-05-19 14:44:32 +0700

    feat: play trailer at banner

--- server/src/services/movie.service.js
+++ server/src/services/movie.service.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-useless-catch */
 import { env } from '../config/environment.config.js'
 
 export const getPopularMovies = async () => {
-  // eslint-disable-next-line no-useless-catch
   try {
     const response = await fetch('https://api.themoviedb.org/3/movie/popular', {
       method: 'GET',
@@ -14,6 +14,27 @@ export const getPopularMovies = async () => {
       throw new Error(`TMDB API Error: ${response.status}`)
     }
 
+    const data = await response.json()
+    return data
+  } catch (error) {
+    throw error
+  }
+}
+export const getMoviesVideoTrailer = async (movieId) => {
+  try {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?language=en-US`,
+      {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${env.TMDB_ACCESS_TOKEN}`,
+        },
+      },
+    )
+    if(!response.ok){
+       throw new Error(`TMDB API Error: ${response.status}`)
+    }
     const data = await response.json()
     return data
   } catch (error) {
